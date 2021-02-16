4ac09de489ff2649d0078ef093c6136ffd2ddaf4
Amrita Rani <arani@itron.com>
2021-02-16 10:09:26 +0530

    Created Spring Boot application to develop REST APIs

---
+++ myretailapp/src/main/java/com/arani/myretailapp/MyretailappApplication.java
@@ -0,0 +1,15 @@
+package com.arani.myretailapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyretailappApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MyretailappApplication.class, args);
+    }
+
+}
---
+++ myretailapp/src/test/java/com/arani/myretailapp/MyretailappApplicationTests.java
@@ -0,0 +1,15 @@
+package com.arani.myretailapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyretailappApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
